e004bf712d9c05fed4021dd0e45dae21eb286c40
akankshajain18 <cs.akankshajain@gmail.com>
2021-01-07 17:45:47 +0530

    SLING-10043 - Naming OSGi configs with dashes breaks the OSGi installer
    rombert: fixed indentation
    
    Closes #6

--- src/main/java/org/apache/sling/installer/factories/configuration/impl/ConfigUpdateHandler.java
+++ src/main/java/org/apache/sling/installer/factories/configuration/impl/ConfigUpdateHandler.java
@@ -121,12 +121,14 @@ public class ConfigUpdateHandler implements ResourceUpdater {
             //format assumption ::: "factory_pid.factory_pid.pid"
             // split pid with lastIndexOf('.') then remove the duplicate factory_pid part from the remaining string using the middle dot split index
             final int lastDotIndex = oldId.lastIndexOf('.');
-            final String factoryIdString = oldId.substring(0, lastDotIndex + 1); // keep it +1 to have last dot intact
-                                                                                 // so that we always have even dots in
-                                                                                 // the string
-            factoryPid = oldId.substring(0, getMiddleDotSplitIndex(factoryIdString));
+            if(lastDotIndex < 0) { //when oldId does not contain any dot
+                factoryPid = oldId;
+            } else {
+                final String factoryIdString = oldId.substring(0, lastDotIndex + 1); // keep it +1 to have last dot intact
+                                                                                      // so that we always have even dots in the string
+                factoryPid = oldId.substring(0, getMiddleDotSplitIndex(factoryIdString));
+            }
             pid = oldId.substring(lastDotIndex+1);
-
         }
 
         return new String[] { factoryPid, pid };
